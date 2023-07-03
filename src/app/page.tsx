ded84a1402b5028f45958ae48bbaa15cba3be7ba
ashraffurqan <132605717+ashraffurqan@users.noreply.github.com>
2023-07-03 21:18:32 +0500

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react"
-import Hero from "./component/hero"
+// import Hero from "./component/hero"
 import Contact from "./component/contact"
-import Project from "./component/project"
+// import Project from "./component/project"
 import About from "./component/about"
 import Skill from "./component/skill"
 
