8142df9543ee9e1c289a40a99054ffe2c927307b
ashraffurqan <132605717+ashraffurqan@users.noreply.github.com>
2024-03-02 02:15:06 +0500

    Update skill.tsx

--- src/app/component/skill.tsx
+++ src/app/component/skill.tsx
@@ -135,7 +135,7 @@ export const Skill = () => {
           </div>
         </div>
       </div>
-   {/* first skill 4 */}
+   {/* first skill 5 */}
    <div className="p-4 w-[100%] md:w-1/3">
         <div className="flex rounded-lg h-full  p-8 flex-col">
           <div className="flex items-center mb-3">
@@ -159,7 +159,101 @@ export const Skill = () => {
         </div>
       </div>
 
+   {/* first skill 4 */}
+   <div className="p-4 w-[100%] md:w-1/3">
+        <div className="flex rounded-lg h-full  p-8 flex-col">
+          <div className="flex items-center mb-3">
+            <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
+            <AiOutlineCheckSquare className='text-xl' />
+            </div>
+            <h2 className="text-gray-900 text-lg title-font font-medium">
+              Python
+            </h2>
+          </div>
+          <div className="flex-grow">
+            
+            <div className='relative h-1 w-full bg-gray-300 rounded-xl font-bold'>
+                <div className='absolute bg-blue-500  h-1 rounded-xl w-[70%]'>
 
+                </div>
+<p className='font-bold text-blue-500 text-right'>65%</p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+         {/* first skill 6 */}
+   <div className="p-4 w-[100%] md:w-1/3">
+        <div className="flex rounded-lg h-full  p-8 flex-col">
+          <div className="flex items-center mb-3">
+            <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
+            <AiOutlineCheckSquare className='text-xl' />
+            </div>
+            <h2 className="text-gray-900 text-lg title-font font-medium">
+              Open AI
+            </h2>
+          </div>
+          <div className="flex-grow">
+            
+            <div className='relative h-1 w-full bg-gray-300 rounded-xl font-bold'>
+                <div className='absolute bg-blue-500  h-1 rounded-xl w-[70%]'>
+
+                </div>
+<p className='font-bold text-blue-500 text-right'>60%</p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+       {/* first skill 7 */}
+       <div className="p-4 w-[100%] md:w-1/3">
+        <div className="flex rounded-lg h-full  p-8 flex-col">
+          <div className="flex items-center mb-3">
+            <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
+            <AiOutlineCheckSquare className='text-xl' />
+            </div>
+            <h2 className="text-gray-900 text-lg title-font font-medium">
+              Custom GPT turbo 4.0
+            </h2>
+          </div>
+          <div className="flex-grow">
+            
+            <div className='relative h-1 w-full bg-gray-300 rounded-xl font-bold'>
+                <div className='absolute bg-blue-500  h-1 rounded-xl w-[70%]'>
+
+                </div>
+<p className='font-bold text-blue-500 text-right'>60%</p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+  {/* first skill 7 */}
+  <div className="p-4 w-[100%] md:w-1/3">
+        <div className="flex rounded-lg h-full  p-8 flex-col">
+          <div className="flex items-center mb-3">
+            <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-blue-500 text-white flex-shrink-0">
+            <AiOutlineCheckSquare className='text-xl' />
+            </div>
+            <h2 className="text-gray-900 text-lg title-font font-medium">
+              Fast API SQL Model
+            </h2>
+          </div>
+          <div className="flex-grow">
+            
+            <div className='relative h-1 w-full bg-gray-300 rounded-xl font-bold'>
+                <div className='absolute bg-blue-500  h-1 rounded-xl w-[70%]'>
+
+                </div>
+<p className='font-bold text-blue-500 text-right'>65%</p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
 
 
     </div>
